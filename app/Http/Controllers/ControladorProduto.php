b5fc364447e227fc27c1c4338e918db2110867a7
Darkin <joaovsantos29@outlook.com>
2018-12-11 23:49:56 -0200

    first commit

---
+++ app/Http/Controllers/ControladorProduto.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Produto;
+use App\Categoria;
+
+class ControladorProduto extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $prod = Produto::all();
+        return view('produtos', compact('prod'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $cats = Categoria::all();
+        return view('novosprodutos', compact('cats'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       $prod = new Produto();
+
+        $prod->nome=$request->input('nomeProduto');
+        $prod->estoque=$request->input('estoque');
+        $prod->preco=$request->input('preco');
+        $prod->id_categoria=$request->input('idCat');
+        $prod->save();
+        return redirect('/produtos');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+       $prod = Produto::find($id);
+        if(isset($prod)){
+            return view('editarProduto' ,compact('prod'));
+        }
+        return redirect('/produtos');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $prod = Produto::find($id);
+        if(isset($prod)){
+            $prod->delete();
+        }
+        return redirect('/produtos');
+    }
+}
